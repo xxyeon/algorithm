20f1fec972ac2ddcf7c869b43f6f8c4b9b2fe2a9
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2024-03-12 02:07:26 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
@@ -2,20 +2,14 @@ n = int(input())
 data = list(map(int, input().split()))
 y = 0
 m = 0
-yR = 0
-mR = 0
 for i in data:
-  y +=  10 * (i // 30)
-  yR = i % 30
-  m += 15 * (i // 60)
-  mR = i % 60
-  if 0 <= yR <= 29:
-    y += 10
-  if 0 <= mR <= 59:
-    m += 15
+  y +=  10 * (i // 30) + 10
+  m += 15 * (i // 60) + 15
+  
 if y < m:
   print("Y", y)
 elif y > m:
   print("M", m)
 else:
   print("Y M", y)
+  
