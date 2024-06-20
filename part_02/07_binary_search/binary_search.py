45c90fce6cabd034ed9d20ffd91c749bdaff866d
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2024-06-20 21:50:39 +0900

    Add files via upload

---
+++ part_02/07_binary_search/binary_search.py
@@ -0,0 +1,26 @@
+#이진 탐색
+# sol 1) 재귀함수
+def binary_search(array, target, start, end):
+  if start > end: #tartget이 array에 없는 경우
+    return None
+  mid = (start + end) // 2
+  if array[mid] == target:
+    return mid
+  elif array[mid] > target:
+    return bianry_search(array, start, mid-1)
+  elif array[mid] < target:
+    return binary_search(array, mid+1, end)
+
+# sol 2) 반복문
+def binary_search2(array, target, start, end):
+  while start <= end:
+    mid = (start + end) // 2
+    if array[mid] == tartget:
+      return mid
+    elif array[mid] > target:
+      end = mid - 1
+    elif array[mid] < target:
+      start = mid + 1
+  return None
+
+
