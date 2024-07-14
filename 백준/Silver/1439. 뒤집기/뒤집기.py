9932c50e231410130b3457e39ea270d4a406e877
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2024-07-14 14:23:15 +0900

    [Silver V] Title: 뒤집기, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,8 +1,16 @@
-import sys
-s = sys.stdin.readline()
+#최소 횟수 -> 그리디
+# 0과 1의 연속된 문자열의 수를 세서 적은 수가 답
+#0001100101 이라면 010101이라고 생각할 수 있음.
+s = input()
+zero = s.split('1')
+one = s.split('0')
+cnt_one = 0
+cnt_zero = 0
+for i in zero:
+  if i != '':
+    cnt_zero += 1
 
-lst = [s[0]]
-for i in range(len(s)-1):
-  if s[i] != s[i+1]:
-    lst.append(s[i+1])
-print(lst.count("1")) if(lst.count("0") > lst.count("1")) else print(lst.count("0"))
+for i in one:
+  if i != '':
+    cnt_one += 1
+print(min(cnt_one, cnt_zero))
