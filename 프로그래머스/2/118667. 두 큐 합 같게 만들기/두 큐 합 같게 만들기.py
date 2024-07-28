fe6e20517e2193efa770cdd12ad9c2139c07c244
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2024-07-28 16:47:29 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 64.23 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,18 +1,60 @@
-from collections import deque
+
+
 def solution(queue1, queue2):
-    q1, q2 = deque(queue1), deque(queue2)
-    q1_sum, q2_sum = sum(q1), sum(q2)
-    for i in range(300000):
-        if q1_sum == q2_sum:
-            return i
-        elif q1_sum > q2_sum:
-            num = q1.popleft()
-            q2.append(num)
-            q1_sum -= num
-            q2_sum += num
-        else: # q2_sum이 q1_sum보다 클 때
-            num = q2.popleft()
-            q1.append(num)
-            q2_sum -= num
-            q1_sum += num      
-    return -1 # 값이 같아지지 않으면 -1을 반환
+    answer = -2
+    #300000 * 300000
+    lst = [0] * 300000
+    cnt = 0
+    min_cnt = 1e10
+    total_sum = sum(queue1) + sum(queue2)
+    #각 큐의 합을 구한후 total_sum/2가 되려면 어느 수를 빼야하는지...
+    sum_q1 = sum(queue1)
+    sum_q2 = sum(queue2)
+    need_q1 = total_sum//2 - sum_q1
+    q1_sum = sum(queue1)
+    need_q2 = total_sum//2 - sum_q2
+    q1_idx = 0
+    
+#     while True:
+#         if len(queue2) > 0:
+#             # queue1.append(queue2.pop(0))
+#             total_sum += queue2.pop(0)
+#             cnt += 1
+        
+#         if total_sum//2 == sum(queue1):
+#             answer = cnt
+#             break
+#         elif total_sum//2 < sum(queue1):
+#             n = queue1.pop(0)
+#             total_sum -= n
+#             cnt += 1
+#         elif total_sum//2 > sum(queue1):
+#             continue
+            
+    print("total_sum:", total_sum//2)
+    for i in range(len(queue2) * 2):
+        if len(queue1) == 0 and lst[i-1] == 0: #이전에 값을 넣은게 없고 queue1이 empty라면
+            answer = -1
+            break
+        if total_sum//2 > q1_sum:
+            if len(queue2) > 0:
+                # queue1.append(queue2.pop(0))
+                q2 = queue2.pop(0)
+                q1_sum += q2
+                cnt += 1
+                print(q1_sum)
+                print("queue2.pop(0): ", q2)
+
+        elif total_sum//2 < q1_sum:
+            n = queue1.pop(0)
+            lst[i] = n
+            q1_sum -= n
+            cnt += 1
+            print(q1_sum)
+        elif total_sum//2 == q1_sum:
+            answer = cnt
+    
+            
+            
+                            
+    return answer
