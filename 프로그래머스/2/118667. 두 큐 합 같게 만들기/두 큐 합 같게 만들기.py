a9112dcbdc6b39f8a513a6dda94ba03d594d18ae
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2024-07-28 19:14:45 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 166.27 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,44 +1,37 @@
 from collections import deque
 
 def solution(queue1, queue2):
-    total_sum = sum(queue1) + sum(queue2)
+    answer = -1#-2로 설정하면 for 문에서 첫번째 조건을 만족하지 않으면 -2가 출력되서 실패함 테케 11, 28
+  
+    #300000 * 300000
     
-    # 총 합이 홀수이면 동일하게 나누는 것이 불가능함
-    if total_sum % 2 != 0:
-        return -1
-    
-    target_sum = total_sum // 2
-    
-    # 큐를 deque로 변환
     queue1 = deque(queue1)
     queue2 = deque(queue2)
-    
+    total = sum(queue1) + sum(queue2)
+    if total % 2 != 0:
+        return -1
+    total_div_2 = total // 2
+    #각 큐의 합을 구한후 total_sum/2가 되려면 어느 수를 빼야하는지...
     sum_q1 = sum(queue1)
-    
-    # 최대 연산 횟수 설정
-    max_operations = len(queue1) + len(queue2) * 2
-    i = 0
-    q1_index = 0
-    q2_index = 0
 
-    while i < max_operations:
-        if sum_q1 == target_sum:
+    # while True:
+    for i in range(300000): #총 배열의 최대 길이가 300000 while로 하면 테케 3번에 무한 루프..?
+        if len(queue2) == 0 or len(queue1) == 0:
+            print(queue2, queue1)
+            return -1
+
+        elif total_div_2 == sum_q1:
             return i
-        elif sum_q1 > target_sum:
-            if q1_index < len(queue1) + len(queue2):  # queue1이 비어있지 않은 경우에만 수행
-                n = queue1.popleft()
-                sum_q1 -= n
-                queue2.append(n)
-            else:
-                return -1
-        else:
-            if q2_index < len(queue1) + len(queue2):  # queue2가 비어있지 않은 경우에만 수행
-                n = queue2.popleft()
-                sum_q1 += n
-                queue1.append(n)
-            else:
-                return -1
-        i += 1
-    
-    return -1
+        elif total_div_2 < sum_q1:
+            n = queue1.popleft()
+            queue2.append(n)
+            sum_q1 -= n
+            
+        elif total_div_2 > sum_q1:
+            n = queue2.popleft()
+            queue1.append(n)
+            sum_q1 += n
+        
 
+    return answer
+                            
