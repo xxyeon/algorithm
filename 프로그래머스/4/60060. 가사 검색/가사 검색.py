7b64f7229a725ee60d6afb11da5076f89249c0e7
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2024-08-11 01:04:01 +0900

    [level 4] Title: 가사 검색, Time: 288.44 ms, Memory: 27.6 MB -BaekjoonHub

--- 프로그래머스/4/60060. 가사 검색/가사 검색.py
+++ 프로그래머스/4/60060. 가사 검색/가사 검색.py
@@ -1,32 +1,46 @@
-from bisect import bisect_left, bisect_right
+def binary_search(start, end, target, words):
+    while start < end:
+        mid = (start + end) // 2
+        if words[mid] < target:
+            start = mid + 1
+        else:
+            end = mid
+    return start
 
-
-def count_by_range(a, left_value, right_value):
-    right_idx = bisect_right(a, right_value)
-    left_idx = bisect_left(a, left_value)
-    return right_idx - left_idx
-#최대 단어길이 10000
-array = [[] for _ in range(10001)]  #단어 길이대로 저장할 리스트
-reverse = [[] for _ in range(10001)]
 def solution(words, queries):
     answer = []
-    #단어 길이대로 저장하기
+    words.sort()
+    
+    # 원래 단어와 뒤집힌 단어를 길이에 따라 분류
+    words_by_length = [[] for _ in range(10001)]
+    reversed_words_by_length = [[] for _ in range(10001)]
+    
     for word in words:
-        array[len(word)].append(word)
-        reverse[len(word)].append(word[::-1])
-        
-    #각 배열에 담긴 가사 정렬
+        words_by_length[len(word)].append(word)
+        reversed_words_by_length[len(word)].append(word[::-1])
+    
+    # 각 길이별 단어 목록을 정렬
     for i in range(10001):
-        array[i].sort()
-        reverse[i].sort()
+        words_by_length[i].sort()
+        reversed_words_by_length[i].sort()
+
+    for query in queries:
+        if query[0] == '?':  # 접미사일 경우
+            left_query = query.replace('?', 'a')[::-1]
+            right_query = query.replace('?', 'z')[::-1]
+            words_to_search = reversed_words_by_length[len(query)]
+        else:  # 접두사일 경우
+            left_query = query.replace('?', 'a')
+            right_query = query.replace('?', 'z')
+            words_to_search = words_by_length[len(query)]
         
-    #가사 검색 시작
-    for i in queries:
-        #i가 포함된 가사 수가 words에 총 몇개있는지 ...
-        if i[0] != "?": #와일드카드가 접두사이면
-            cnt = count_by_range(array[len(i)], i.replace("?", "a"), i.replace("?", "z"))
-        else:
-            cnt = count_by_range(reverse[len(i)], i[::-1].replace("?", "a"), i[::-1].replace("?", "z"))
-        answer.append(cnt)
-                
+        # 이진 탐색으로 범위 찾기
+        left_idx = binary_search(0, len(words_to_search), left_query, words_to_search)
+        right_idx = binary_search(0, len(words_to_search), right_query, words_to_search)
+
+        answer.append(right_idx - left_idx)
+
     return answer
+
+# 예제 실행
+print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
