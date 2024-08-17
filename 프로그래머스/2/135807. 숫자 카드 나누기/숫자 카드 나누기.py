87b72da4a2c0d080b1c6470b428e32b9443943d7
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2024-08-17 17:08:14 +0900

    Refactor: 조건 검사하는 로직 변경

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -1,7 +1,9 @@
 import math
 
+#all 함수 사용해서 반복문이 모두 참인지 확인 가능
+
 def solution(arrayA, arrayB):
-    answer = []
+    answer = 0
     #가장큰 양의 정수 a
     #1. arrayA에서 작은거 기준으로 a, b 나눠보고 조건확인
     #2. arrayAB에서 작은거 기준으로 a, b나눠보고 조건확인
@@ -16,32 +18,18 @@ def solution(arrayA, arrayB):
     result = True
 
     #a_num으로는 B, b_num으로는 A 나눌 수 없는지검사
-#     for i, j in zip(arrayA, arrayB):
+    for i, j in zip(arrayA, arrayB):
 
-#         if i % num_b == 0 and j % num_a == 0:#조건 불만족
-#             result = False
-#             break
-    result = True
-    for i in arrayA:
-        if i % num_b == 0:
-            result = False
-            break
-    if result != False:
-        answer.append(num_b)
-    
-    result = True
-    for i in arrayB:
-        if i % num_a == 0:
-            result = False
+        # if i % num_b == 0 and j % num_a == 0:#조건 불만족
+        #     result = False
+        #     break
+        if num_b != 0 and i % num_b == 0: #num_b는 조건 불만족
+            num_b = 0
+        if num_a != 0 and j % num_a == 0:
+            num_a = 0
 
-    
-    if result != False:
-        answer.append(num_a)
 
-    if len(answer) == 0:
-        answer = 0
-    else:
-        answer = max(answer)
+    answer = max(num_a, num_b)
    
 
 
