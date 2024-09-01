b3e74596a51ae54d6c628cb42b52700c88747a70
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2024-09-01 21:35:24 +0900

    [Silver I] Title: 회의실 배정, Time: 328 ms, Memory: 122680 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,19 +1,15 @@
 n = int(input())
-lst = []
-for _ in range(n):
+time = []
+cnt = 0
+for i in range(n):
   start, end = map(int, input().split())
-  lst.append([start, end])
-
-lst.sort(key=lambda x: (x[1], x[0]))
-
-cnt = 1
-start = lst[0][0]
-end = lst[0][1]
-for i in range(n - 1):
-  if end > lst[i + 1][0]:
+  time.append((start, end))
+time = sorted(time, key = lambda x:(x[1], x[0]))
+now_end = 0
+for i in range(n):
+  start, end = time[i]
+  if now_end > start:
     continue
-
   cnt += 1
-  end = lst[i + 1][1]
-
+  now_end = end
 print(cnt)
