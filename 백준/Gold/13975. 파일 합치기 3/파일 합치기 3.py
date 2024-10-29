03b6b7d330bb75f7be2db107374e2a3149d2a057
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2024-10-30 00:42:10 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 4912 ms, Memory: 185924 KB -BaekjoonHub

---
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.py
@@ -0,0 +1,18 @@
+#새로운 파일이 생기면서 기존에 있는 파일들의 비용과 새로 만들어진 파일 비용을 비교해야하므로 우선순위 큐 사용
+import heapq
+t = int(input())
+total_cost = []
+for _ in range(t):
+    cost = 0
+    q = []
+    k = int(input())
+    files = list(map(int, input().split()))
+    for i in range(k):
+        heapq.heappush(q, files[i])
+    while len(q) > 1:
+        cost1 = heapq.heappop(q)
+        cost2 = heapq.heappop(q)
+        cost += cost1+cost2 #새파일 만드는데 발생하는 비용
+        
+        heapq.heappush(q, cost1+cost2)
+    print(cost)
