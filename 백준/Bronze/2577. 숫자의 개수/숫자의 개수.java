edabcaa4a897dc6978489a7bb87b68ce7bbbafa9
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2025-01-13 17:35:37 +0900

    [Bronze II] Title: 숫자의 개수, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -2,30 +2,27 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         long[] lst = new long[3];
         int[] num = new int[10];
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        // 입력받기
         for (int i = 0; i < 3; i++) {
             lst[i] = Long.parseLong(bf.readLine().trim());
         }
 
-        // 곱셈 결과 계산
         long result = 1;
         for (long i : lst) {
             result *= i;
         }
 
-        // 결과 숫자 카운팅
-        String str = Long.toString(result);
+        String str = Long.toString(result); //메모
+
         for (char digit : str.toCharArray()) {
             num[digit - '0']++;
         }
-
-        // 결과 출력
-        for (int count : num) {
-            System.out.println(count);
+        for (int i : num) {
+            System.out.println(i);
         }
+
     }
 }
