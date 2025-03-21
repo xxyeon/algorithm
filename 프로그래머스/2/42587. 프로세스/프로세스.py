b80a48712ba67dbc0bfa5db9fd18e19983fbf893
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2025-03-21 15:37:01 +0900

    [level 2] Title: 프로세스, Time: 0.37 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -8,17 +8,15 @@ def solution(priorities, location):
         #큐에 우선순위가 더 높은게 있다는 걸 어떻게 알 수 있을까?
         #큐에서 하나 꺼낼때마다 순회하면서 봐야하나? -> O(N^2) 프로세스 개수가 100이하니까 가능
     turn = 0
-    while len(priorities) > 1:
+    while priorities:
         priority, idx = priorities.popleft()
         for p, i in priorities:
             if priority < p:
                 priorities.append((priority, idx))
                 #다시 큐에 넣고, 다시 pop()
                 break
-        if (priority, idx) != priorities[-1]: #꺼낸 프로세스가 큐에 다시 들어가지 않았을 경우
+        else: #꺼낸 프로세스가 큐에 다시 들어가지 않았을 경우
             turn += 1
             if idx == location:
                 return turn
-    if priorities:
-        return turn+1
     
