b25339bf55da7c66bc2f6a0684780ad63f96e0b4
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2025-04-04 22:42:58 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,20 +1,17 @@
 def solution(n, lost, reserve):
-    lost.sort()
-    reserve.sort()
-
-    # 여벌 체육복이 있지만 도난당한 학생 찾기
+    
     both = set(lost) & set(reserve)
     lost = list(set(lost) - both)
     reserve = list(set(reserve) - both)
-
+    lost.sort()
     answer = n - len(lost)
-
     for l in lost:
-        if l - 1 in reserve:
-            reserve.remove(l - 1)
+        if l-1 in reserve:
+            reserve.remove(l-1)
             answer += 1
-        elif l + 1 in reserve:
-            reserve.remove(l + 1)
+            continue
+        if l+1 in reserve:
+            reserve.remove(l+1)
             answer += 1
 
     return answer
