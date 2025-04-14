a120b9683b9ac60bf47d76e71038ef5ef4806bd2
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2025-04-14 15:38:36 +0900

    [Silver IV] Title: 상근이의 여행, Time: 3676 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -1,32 +1,11 @@
-import sys
-from collections import deque
-
-def bfs(start, visited, graph):
-    q = deque()
-    q.append(start)
-    visited[start] = True
-    cnt = 0
-    while q:
-        now = q.popleft()
-        for next in graph[now]:
-            if not visited[next]:
-                visited[next] = True
-                cnt += 1
-                q.append(next)
-    return cnt
-
 test = int(input())
 
 for _ in range(test):
     n, m = map(int, input().split())
-    MAP = [[] for i in range(n+1)]
-    visited = [False] * (n+1)
+   
 
     #그래프 그리기
     for _ in range(m):
         a, b = map(int, input().split())
-        MAP[a].append(b)
-        MAP[b].append(a)
-    answer = bfs(1, visited, MAP)
-    print(answer)
+    print(n-1)
 
