8450bf7e7a241279eaf42a05083e8cb33441ae3f
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2025-04-28 22:03:42 +0900

    [Silver II] Title: 싸이버개강총회, Time: 504 ms, Memory: 54384 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
@@ -3,44 +3,36 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        //시작시간에 들어왔는지, 끝나는 시간에 남았는지,
-        //시작시간에 들어왔는지: 스트리밍 시작~ 총회 시작포함하여확인, 세트에 넣기
-        //끝나는 시간에 나갔는지: 총회 종료 포함 ~ 스트리밍 종료, 세트에서 빼기 -> 출석
-        Set<String> set = new HashSet<>();
+        String[] times = br.readLine().split(" ");
+        String S = times[0]; // 개강 총회 시작 시간
+        String E = times[1]; // 개강 총회 종료 시간
+        String Q = times[2]; // 스트리밍 종료 시간
+
         Set<String> attendance = new HashSet<>();
-        String[] times = bf.readLine().split(" ");
-        String[] start = times[0].split(":");
-        String[] end = times[1].split(":");
-        String[] endStreaming = times[2].split(":");
-
-        int startTime = Integer.parseInt(start[0] + start[1]);
-        int endTime = Integer.parseInt(end[0] + end[1]);
-        int endSt = Integer.parseInt(endStreaming[0] + endStreaming[1]);
+        Set<String> leave = new HashSet<>();
 
         String input;
-        int cnt = 0;
-        while ((input = bf.readLine()) != null) {
-            if (input.isEmpty()) break;
-            String[] chat = input.split(" ");
-            String[] time = chat[0].split(":");
+        while ((input = br.readLine()) != null) {
+            String[] log = input.split(" ");
+            String time = log[0];
+            String name = log[1];
 
-            int timeInt = Integer.parseInt(time[0] + time[1]);
-            String name = chat[1];
-            if (startTime >= timeInt) {
-                //시작시간에 들어옴: 출석
-                set.add(name);
-            } else if(set.contains(name)){
-                //출석한 사람이 나가는 시간확인 여러번 나가는 경우도 있음 set로 동일한 이름이 나가면 한번만 출석되도록
-                if(endTime <= timeInt && timeInt <= endSt) {
-                    attendance.add(name);
-                }
+            if (time.compareTo(S) <= 0) { 
+                attendance.add(name);
+            } else if (time.compareTo(E) >= 0 && time.compareTo(Q) <= 0) {
+                leave.add(name);
             }
         }
-        System.out.print(attendance.size());
 
-        bf.close();
+        int count = 0;
+        for (String name : attendance) {
+            if (leave.contains(name)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
     }
 }
