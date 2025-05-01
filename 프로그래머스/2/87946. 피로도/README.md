8bb27a4095dae93aa63dd4cb1e28031be6f45b89
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2025-05-01 13:52:24 +0900

    [level 2] Title: 피로도, Time: 2.94 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 78.9 MB, 시간: 5.91 ms
+메모리: 89.5 MB, 시간: 2.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 01일 13:44:18
+2025년 05월 01일 13:52:22
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -12,17 +12,17 @@ class Solution {
     public void permutation(int tired, int[][] dungenons, boolean[] visited, int cnt) { //cnt: 던전 탐험한 수
         
         for (int i = 0; i < dungenons.length; i++) {
-            if (tired < dungenons[i][0]) {
-                max = Math.max(max, cnt);
-            }
-            else if (!visited[i]) {
+            // if (tired < dungenons[i][0]) {
+            //     max = Math.max(max, cnt);
+            // }
+           if (!visited[i]) {
                 if (tired >= dungenons[i][0]) {
                     visited[i] = true;
                     permutation(tired-dungenons[i][1], dungenons, visited, cnt + 1);
                     visited[i] = false;
                 }   
             }
-            
         }
+        max = Math.max(max, cnt);
     }
 }
