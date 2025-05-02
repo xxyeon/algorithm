8c9421e80d49bfa19cb928bfc0ed0cd96c34262e
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2025-05-02 18:46:47 +0900

    [level 3] Title: N으로 표현, Time: 50.07 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,51 +1,39 @@
-import java.util.Set;
 import java.util.HashSet;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.Set;
 class Solution {
     public int solution(int N, int number) {
-        int answer = 0;
-        boolean isFound = false;
-        
-        List<Set<Integer>> dp = new ArrayList<>();
-        dp.add(new HashSet<>());
-        dp.get(0).add(0);
-       
-        for (int i = 1; i < 9; i++) {
-            dp.add(new HashSet<>());
-            int base = Integer.parseInt(String.valueOf(N).repeat(i)); 
-            dp.get(i).add(base);
-        
-            
-            
-            //i개 사용해서 만들수 있는 수
-            for(int j = 0; j < i/2+1; j++){
-                
-                for (int n : dp.get(j)) {
-                    for (int num : dp.get(i-j)) {
-                        dp.get(i).add(n + num);
-                        dp.get(i).add(n - num);
-                        dp.get(i).add(num - n);
-                        if (num != 0) dp.get(i).add((int)(n / num));
-                        if (n != 0) dp.get(i).add((int)(num/n));
-                        dp.get(i).add(n * num);
+       int answer = -1;
+        Set<Integer>[] setArr = new Set[9];
+        int t = N;
+        for(int i = 1; i < 9; i++) {
+            setArr[i] = new HashSet<>();
+            setArr[i].add(t);
+            t = t * 10 + N;
+        }
+        for(int i = 1; i < 9; i++) {
+            for(int j = 1; j < i; j++) {
+                for(Integer a : setArr[j]) {
+                    for(Integer b : setArr[i - j]) {
+                        setArr[i].add(a + b);
+                        setArr[i].add(a - b);
+                        setArr[i].add(b - a);
+                        setArr[i].add(a * b);
+                        if(b != 0) {
+                            setArr[i].add(a / b);
+                        }
+                        if(a != 0) {
+                            setArr[i].add(b / a);
+                        }
                     }
-                    
                 }
-                
             }
-            if (dp.get(i).contains(number)){
-                isFound = true;
+        }
+        for(int i = 1; i < 9; i++) {
+            if(setArr[i].contains(number)) {
                 answer = i;
                 break;
             }
-        
-
-                
-            }
-        
-        
-        return isFound ? answer : -1;
+        }
+        return answer;
     }
 }
