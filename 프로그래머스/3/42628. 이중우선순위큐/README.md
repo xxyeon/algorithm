32e6656ada4951ab5a28204aa16e79e303b8cd4d
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2025-05-02 16:13:05 +0900

    [level 3] Title: 이중우선순위큐, Time: 55.32 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
 
 ### 성능 요약
 
-메모리: 121 MB, 시간: 135.54 ms
+메모리: 109 MB, 시간: 55.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 02일 15:40:12
+2025년 05월 02일 16:13:02
 
 ### 문제 설명
 
--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,76 +1,26 @@
-import java.util.PriorityQueue;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] operations) {
-        
-        //최대우선순위와 최소우선순위 두고 나중에 집합 연산 (uion)
-        PriorityQueue<Integer> maxQ = new PriorityQueue<>((a, b) -> b-a);
-        PriorityQueue<Integer> minQ = new PriorityQueue<>((a, b) -> a-b);
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        
-        for(String op : operations) {
-            String[] arr = op.split(" ");
-            int n = Integer.parseInt(arr[1]);
-            switch (arr[0]) {
-                case "I":
-                    maxQ.add(n);
-                    minQ.add(n);
-                    if (!map.containsKey(n)) {
-                        map.put(n, 1);
-                    } else {
-                        map.put(n, map.get(n) + 1);
-                    }
-                    
-                    break;
+        Queue<Integer> minpq = new PriorityQueue<>();
+        Queue<Integer> maxpq = new PriorityQueue<>(Collections.reverseOrder());
 
-                case "D":
-                    if (n == 1) {
-                        //0이아닌 값이 나올 때 까지 while돌려야함
-                        while (!maxQ.isEmpty() && map.get(maxQ.peek()) == 0) {
-                            maxQ.poll();
-                        }
-                        if (!maxQ.isEmpty()) {
-		                    map.put(maxQ.peek(), map.get(maxQ.peek()) - 1);
-                            maxQ.poll();
-                       
-                        }
-                    }
-                    else if (n == -1) {
-                        while (!minQ.isEmpty() && map.get(minQ.peek()) == 0) {
-                            minQ.poll();
-                        }
-                        if (!minQ.isEmpty()) {
-		                    map.put(minQ.peek(), map.get(minQ.peek()) - 1);
-                            minQ.poll();
-                       
-                        }
-				            
-                    }
-                    break;
-                }
-        }
-  
-        int sum = 0;
-        for (Integer n : map.values()) {
-            sum += n;
-        }
-        if (sum == 0) {
-            int[] answer = {0,0};
-            return answer;
-        }
-        
-        int max = 0;
-        int min = maxQ.peek();
-        
-        for (Integer n : map.keySet()) {
-            if (map.get(n) >= 1) {
-                max = Math.max(max, n);
-                min = Math.min(min, n);
+        for (String operation : operations) {
+            if (operation.startsWith("I ")) {
+                int n = Integer.parseInt(operation.substring(2));
+                minpq.offer(n);
+                maxpq.offer(n);
+            } else if (!minpq.isEmpty() && operation.equals("D -1")) {
+                maxpq.remove(minpq.poll());
+            } else if (!maxpq.isEmpty() && operation.equals("D 1")) {
+                minpq.remove(maxpq.poll());
             }
         }
-        int[] answer = {max, min};
-        return answer;
+
+        if (minpq.isEmpty() && maxpq.isEmpty()) {
+            return new int[]{0, 0};
+        }
+
+        return new int[]{maxpq.poll(), minpq.poll()};
     }
 }
