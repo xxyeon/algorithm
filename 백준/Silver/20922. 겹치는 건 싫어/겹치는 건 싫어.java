fe1ef443d028099265b894d4426df50d8f76249f
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2025-05-06 13:08:28 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 524 ms, Memory: 62612 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -1,49 +1,31 @@
 import java.io.*;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		String[] input = bf.readLine().split(" ");
-		int n = Integer.parseInt(input[0]);
-		int k = Integer.parseInt(input[1]);
-		int[] arr = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		int max = Integer.MIN_VALUE;
-		Map<Integer, Integer> map = new HashMap<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = bf.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+        int[] arr = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-		for(int num : arr) {
-			map.put(num, 0);
-		}
+        Map<Integer, Integer> freq = new HashMap<>();
+        int start = 0, end = 0, maxLength = 0;
 
-		int start = 0;
-		int end = 0;
-		while(end < n) {
-			// System.out.println("end: " +  end );
-			if (map.get(arr[end]) + 1 > k) {
-				// System.out.println("start: " + start);
-				while (true) {
-					if(arr[start] == arr[end]) { //end를 옮길 수 있게 start가 가리키는 정수가 k개 안넘도록
-						// System.out.println("map.get(arr[end]): " + map.get(arr[end]));
-						// System.out.println("map.get(arr[start]): " + map.get(arr[start]));
-						map.put(arr[start], map.get(arr[start]) - 1);
-						start++;
-						break;
-					}
-					map.put(arr[start], map.get(arr[start]) - 1);
-					start++;
-				}
-			}
-			map.put(arr[end], map.get(arr[end]) + 1);
-			end++;
-			max = Math.max(max, end - start); //start 포함 ~ end-1 포함
+        while (end < n) {
+            int current = arr[end];
+            freq.put(current, freq.getOrDefault(current, 0) + 1);
 
-			// System.out.println(map.toString());
-		}
+            while (freq.get(current) > k) {
+                int left = arr[start];
+                freq.put(left, freq.get(left) - 1);
+                start++;
+            }
 
+            maxLength = Math.max(maxLength, end - start + 1);
+            end++;
+        }
 
-		System.out.println(max);
-	}
-
+        System.out.println(maxLength);
+    }
 }
