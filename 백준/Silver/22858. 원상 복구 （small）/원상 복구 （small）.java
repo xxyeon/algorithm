caa864fff5ae8228ed2728fb76391cf351304318
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2025-05-09 23:43:55 +0900

    [Silver III] Title: 원상 복구 (small), Time: 248 ms, Memory: 19492 KB -BaekjoonHub

--- 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.java
+++ 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.java
@@ -2,39 +2,33 @@ import java.io.*;
 import java.util.Arrays;
 
 public class Main {
-	private static final int[][] d = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
-		String[] input = bf.readLine().split(" ");
-		int n = Integer.parseInt(input[0]);
-		int k = Integer.parseInt(input[1]);
+        String[] input = bf.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
 
-		int[] s = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		int[] d = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		int[] p = new int[n];
-		for(int i =0; i < k; i++) {
-			shuffleReverse(d, s, p);
+        int[] s = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] d = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] p = new int[n];
 
-			for(int j =0; j < n; j++) {
-				s[j] = p[j];
-			}
-		}
+        for (int i = 0; i < k; i++) {
+            shuffleReverse(d, s, p);
+            System.arraycopy(p, 0, s, 0, n);
+        }
 
-		for(int i : p) {
-			sb.append(i).append(" ");
-		}
+        for (int i : p) {
+            sb.append(i).append(" ");
+        }
 
-		System.out.println(sb.toString());
+        System.out.println(sb.toString());
+    }
 
-
-	}
-	private static void shuffleReverse(int[] d, int[] s, int[] p) {
-		for(int i = 0; i < d.length; i++) {
-			p[d[i] - 1] = s[i];
-		}
-	}
-
+    private static void shuffleReverse(int[] d, int[] s, int[] p) {
+        for (int i = 0; i < d.length; i++) {
+            p[d[i] - 1] = s[i];
+        }
+    }
 }
-
