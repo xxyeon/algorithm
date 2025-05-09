db6868d02720d5eb06a94914686fc7e1a702bf54
JiYeon Kim <87413158+xxyeon@users.noreply.github.com>
2025-05-09 21:04:58 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 192 ms, Memory: 22684 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -1,35 +1,31 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Collections;
 
 public class Main {
-	private static final int[][] d = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
+		StringBuilder sb = new StringBuilder();
 
 		char[] line = bf.readLine().toCharArray();
 		boolean isTag = false;
 		List<Character> list = new ArrayList<>();
+
 		for (char ch : line) {
 			if (ch == '<') {
+				// 단어가 끝났다면 뒤집어 추가
+				if (!list.isEmpty()) {
+					sb.append(flip(list));
+					list.clear();
+				}
 				isTag = true;
+				sb.append(ch);
 			} else if (ch == '>') {
 				isTag = false;
 				sb.append(ch);
-				continue;
-			}
-			if (isTag) {
-				if (!list.isEmpty()) {
-					String word = flip(list);
-					sb.append(word);
-					list.clear();
-				}
+			} else if (isTag) {
 				sb.append(ch);
-
-			} else { //태그가 아니라면
+			} else {
 				if (ch == ' ') {
 					sb.append(flip(list)).append(' ');
 					list.clear();
@@ -37,27 +33,21 @@ public class Main {
 					list.add(ch);
 				}
 			}
-
 		}
-		//마지막 단어(마지막엔 공백이 없으므로 sb에 append안됨
+
+		// 마지막 단어 처리
 		if (!list.isEmpty()) {
 			sb.append(flip(list));
 		}
 
-
 		System.out.println(sb.toString());
-
-
 	}
 
 	private static String flip(List<Character> list) {
-		String word = "";
-		for(int i = list.size()-1; i >= 0; i--) {
-			// System.out.println(list.get(i));
-			word += list.get(i);
+		StringBuilder sb = new StringBuilder();
+		for (int i = list.size() - 1; i >= 0; i--) {
+			sb.append(list.get(i));
 		}
-		return word;
+		return sb.toString();
 	}
-
 }
-
