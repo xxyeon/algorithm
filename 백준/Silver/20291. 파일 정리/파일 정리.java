b8c8f3b2adc559c179195cd592345f64e7f6a149
JiYeon Kim <jykim9335@gmail.com>
2025-05-10 00:54:07 +0900

    [Silver III] Title: 파일 정리, Time: 572 ms, Memory: 54940 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -1,32 +1,65 @@
-import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeSet;
+// Authored by : lms0806
+// Co-authored by : -
+// Link : http://boj.kr/a2d39728db0c40a784b657882ee17fc7
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		// StringBuffer sb = new StringBuffer();
+	public static void main(String[] args) throws IOException{
+		FastReader rd = new FastReader();
+		
+		int size = rd.nextInt();
+		
+		TreeMap<String, Integer> map = new TreeMap<>();
+		
+		while(size --> 0) {
+			String str = rd.nextLine();
+			str = str.substring(str.indexOf(".") + 1);
+			
+			map.put(str, map.get(str) == null ? 1 : map.get(str) + 1);
+		}
+		
 		StringBuilder sb = new StringBuilder();
-		Map<String, Integer> map = new HashMap<>();
-
-		int n = Integer.parseInt(bf.readLine());
-		for (int i = 0; i < n; i++) {
-			String[] file = bf.readLine().split("\\.");
-
-			// System.out.println(file[1]);
-			map.put(file[1], map.getOrDefault(file[1], 0) + 1);
-
+		for(String key : map.keySet()) { 
+			sb.append(key).append(" ").append(map.get(key)).append("\n"); 
 		}
-		TreeSet<String> sortedSet = new TreeSet(map.keySet());
-		for (String exe : sortedSet) {
-			sb.append(exe).append(" ").append(map.get(exe)).append('\n');
-		}
-		System.out.print(sb.toString());
-
-
+		System.out.print(sb);
 	}
+	static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
 
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
 
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }
-
