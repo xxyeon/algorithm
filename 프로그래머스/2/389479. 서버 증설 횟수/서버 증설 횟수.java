77d5105d3b57562c510484899eb99f1050eb0643
JiYeon Kim <jykim9335@gmail.com>
2025-05-14 00:24:52 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.03 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,18 +1,20 @@
 class Solution {
     public int solution(int[] players, int m, int k) {
         int answer = 0;
-        int[] server = new int[25];
-        for (int i=0; i<players.length; i++) {
-   
-            if(server[i] < players[i]/m) { //i~i+1 동안 증설된 서버 개수가 모자르면 k동안 사용할 수 있는 서버 증설
-                int initServer = server[i];
-                //k동안 서버 사용가능하도록 증설
-                answer += players[i]/m - server[i];
-                for(int j=0; j < k; j++) {
+        int active = 0;
+        int[] install = new int[24];
+        for(int i=0; i<24; i++) {
 
-                    if (i+j >= 25) break;
-                    server[i+j] += players[i]/m - initServer;
-                }
+            if (i >= k) {//i~i+1
+                //서버 사용 시간이 지났다면
+                active -= install[i-k]; //i-k시간에 증설한 서버 개수 제거
+            }
+            
+            int req = players[i] / m;
+            if (req > active) {
+                install[i] = req - active; //i시간에 증설한 서버 갯수
+                answer += req - active;
+                active += req - active;
             }
         }
         return answer;
