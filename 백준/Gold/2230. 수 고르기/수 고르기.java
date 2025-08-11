bef460e26c7d95052c2c2cc56ad686141c1311bf
JiYeon Kim <jykim9335@gmail.com>
2025-08-11 19:24:30 +0900

    [Gold V] Title: 수 고르기, Time: 312 ms, Memory: 27724 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -1,38 +1,40 @@
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int answer = Integer.MAX_VALUE;
-        String[] input = bf.readLine().split(" ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
         int n = Integer.parseInt(input[0]);
         int m = Integer.parseInt(input[1]);
-
         int[] arr = new int[n];
-        for (int i=0; i < n; i++) {
-            arr[i] = Integer.parseInt(bf.readLine());
+        for(int i=-0; i<n;i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(arr);
         int start = 0;
         int end = 0;
-
-        while((start < n && end < n) && start <= end) {
+        int result = Integer.MAX_VALUE;
+        while(end < n && start <= end) {
             int gap = arr[end] - arr[start];
-            if (gap >= m) {
-                answer = Math.min(gap, answer);
-                start += 1;
-
-            } else if(gap < m) {
+            if(gap < m) {
                 end += 1;
+            } else if(gap >= m) {
+                start += 1;
+                result = Math.min(result, gap);
             }
         }
+        System.out.println(result);
 
-
-        System.out.print(answer);
-        bf.close();
     }
-
 }
+
