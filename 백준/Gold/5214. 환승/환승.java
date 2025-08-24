0d0d25256eb0a4423f51a337f996249af503003f
JiYeon Kim <jykim9335@gmail.com>
2025-08-24 13:39:20 +0900

    [Gold II] Title: 환승, Time: 776 ms, Memory: 142868 KB -BaekjoonHub

--- 백준/Gold/5214. 환승/환승.java
+++ 백준/Gold/5214. 환승/환승.java
@@ -9,6 +9,7 @@ public class Main {
     static ArrayList<ArrayList<Integer>> graph = new ArrayList<>(); //graph[i] => i번 역에 연결된 하이버튜프
     static int[][] hypertube;
     static boolean[] visited;
+    static boolean[] isHypertube;
     static int answer = 0;
     static int[] dis;
     public static void main(String[] args) throws IOException {
@@ -20,6 +21,7 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         hypertube = new int[M][K];
         visited = new boolean[N+1];
+        isHypertube = new boolean[M];
         dis = new int[N+1];
         for (int i = 0; i < N+1; i++) {
             graph.add(new ArrayList<>());
@@ -40,7 +42,6 @@ public class Main {
         q.offer(1);//1번 역에서 시작
         visited[1] = true;
         dis[1] = 1;
-
         while (!q.isEmpty()) {
             int curr = q.poll();
 
@@ -49,6 +50,8 @@ public class Main {
             }
             //curr에 연결된 하이퍼튜브
             for(int num : graph.get(curr)) {
+                if(isHypertube[num]) continue;
+                isHypertube[num] = true;
                 for (int station : hypertube[num]) { //k번
                     if(!visited[station]){
                         visited[station] = true;
