d8589b3c1b59d7012f1364cb0f45363bcaaf5963
JiYeon Kim <jykim9335@gmail.com>
2025-12-02 23:08:57 +0900

    Time: 3 ms (5.75%), Space: 62.1 MB (21.61%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -5,26 +5,26 @@ class Solution {
         //정렬하면 안, 연속된 데이터
         //길이가 1부터 nums.length 까지
 
-        int[] len = new int[nums.length];
-        for(int i = 1;i<=nums.length; i++) {
-            len[i-1] = i;
-        }
-        int start = 0;
-        int end = nums.length-1;
+        // int[] len = new int[nums.length];
+        // for(int i = 1;i<=nums.length; i++) {
+        //     len[i-1] = i;
+        // }
+        int start = 1;
+        int end = nums.length;
         int answer = Integer.MAX_VALUE;
 
         while(start <= end) {
             //이분탐색으로 길이 탐색 -> O(logN)
-            int middle = (start + end) / 2 ;
-            int k = len[middle];
+            int k = (start + end) / 2;
+            // int k = len[middle];
             if(find(k, nums, target)) {
                 answer = Math.min(answer, k);
                 //target보다 크거나 같다 -> 길이가 줄어들어도 된다.
                 //최소길이를 찾아야하므로
-                end = middle - 1;
+                end = k - 1;
             } else {
                 //target보다 작다 -> 길이가 더 길어져야한다
-                start = middle + 1;
+                start = k + 1;
             }       
         }
         answer = answer >= Integer.MAX_VALUE ? 0 : answer;
