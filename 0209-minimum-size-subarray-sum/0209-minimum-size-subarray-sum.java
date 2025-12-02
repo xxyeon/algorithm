59b222f230f499c27ba8d8432085db3bba16bdec
JiYeon Kim <jykim9335@gmail.com>
2025-12-02 23:17:32 +0900

    Time: 1 ms (99.6%), Space: 62 MB (32.28%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,52 +1,20 @@
 
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        //연속적인 비어있지 않은 시퀀스 제일 작은 길이 target보다 크거나 같은 길이, 없으면 0리턴
-        //정렬하면 안, 연속된 데이터
-        //길이가 1부터 nums.length 까지
-
-        // int[] len = new int[nums.length];
-        // for(int i = 1;i<=nums.length; i++) {
-        //     len[i-1] = i;
-        // }
-        int start = 1;
-        int end = nums.length;
-        int answer = Integer.MAX_VALUE;
-
-        while(start <= end) {
-            //이분탐색으로 길이 탐색 -> O(logN)
-            int k = (start + end) / 2;
-            // int k = len[middle];
-            if(find(k, nums, target)) {
-                answer = Math.min(answer, k);
-                //target보다 크거나 같다 -> 길이가 줄어들어도 된다.
-                //최소길이를 찾아야하므로
-                end = k - 1;
-            } else {
-                //target보다 작다 -> 길이가 더 길어져야한다
-                start = k + 1;
-            }       
-        }
-        answer = answer >= Integer.MAX_VALUE ? 0 : answer;
-        
-        return answer;
-    }
-    private boolean find(int k, int[] nums, int target) {
-        int result = 0;
-        //첫번째윈도우
-        for(int i=0;i<k;i++) {
-            result += nums[i];
-        }
-        if(result >= target) {
-            return true;
-        } 
-        //두번째윈도우
-        for(int i=k;i<nums.length;i++){
-            result += nums[i] - nums[i-k];
-            if (result >= target) {
-                return true;
+      
+        int minLen = Integer.MAX_VALUE;
+        int left = 0;
+        int sum = 0;
+        for(int right=0;right<nums.length;right++) {
+            sum += nums[right];
+            while(sum >= target) {
+                minLen = Math.min(minLen, right - left + 1);
+                sum -= nums[left];
+                left += 1;
             }
         }
-        return false;
+        return minLen >= Integer.MAX_VALUE ? 0 : minLen;
+
     }
+  
 }
